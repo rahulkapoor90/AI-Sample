b4f1edd9dcc91ed4cb056ec37a9cd6ee11cfa974
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2016-12-07 22:10:10 +0530

    Minor changes

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,4 +1,6 @@
 'use strict'
+const getCurrentWeather = require('./lib/getCurrentWeather')
+
 const firstOfEntityRole = function(message, entity, role) {
   role = role || 'generic';
 
@@ -16,11 +18,11 @@ exports.handle = function handle(client) {
     },
 
     prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
+      client.addResponse('app:response:name:welcome')
+      client.addResponse('app:response:name:provide/documentation', {
         documentation_link: 'http://docs.init.ai',
       })
-      client.addResponse('provide/instructions')
+      client.addResponse('app:response:name:provide/instructions')
       client.updateConversationState({
         helloSent: true
       })
@@ -34,87 +36,72 @@ exports.handle = function handle(client) {
     },
 
     prompt() {
-      client.addResponse('apology/untrained')
-     client.done()
-    }
-  })
-
-  const handleGreeting = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addTextResponse('Hello world, I mean human')
+      client.addResponse('app:response:name:apology/untrained')
       client.done()
     }
   })
 
-  const handleGoodbye = client.createStep({
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
+
+    extractInfo() {
+     const city = firstOfEntityRole(client.getMessagePart(), 'city')
+      if (city) {
+        client.updateConversationState({
+          weatherCity: city,
+        })
+        console.log('User wants the weather in:', city.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:prompt/weather_city')
+      client.done()
+    },
+  })
+
+  const provideWeather = client.createStep({
     satisfied() {
       return false
     },
 
-    prompt() {
-      client.addTextResponse('See you later!')
-      client.done()
-    }
+    prompt(callback) {
+      getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
+        if (!resultBody || resultBody.cod !== 200) {
+          console.log('Error getting weather.')
+          callback()
+          return
+        }
+
+        const weatherDescription = (
+          resultBody.weather.length > 0 ?
+          resultBody.weather[0].description :
+          null
+        )
+
+        const weatherData = {
+          temperature: resultBody.main.temp,
+          condition: weatherDescription,
+          city: resultBody.name,
+        }
+
+        console.log('sending real weather:', weatherData)
+        client.addResponse('app:response:name:provide_weather/current', weatherData)
+        client.done()
+
+        callback()
+      })
+    },
   })
 
   client.runFlow({
-    classifications: {
-      goodbye: 'goodbye',
-      greeting: 'greeting'
-    },
+    classifications: {},
     streams: {
-      goodbye: handleGoodbye,
-      greeting: handleGreeting,
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained]
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
     }
   })
 }
-
-client.runFlow({
-  classifications: {},
-  streams: {
-    main: 'getWeather',
-    hi: [sayHello],
-    getWeather: [collectCity, provideWeather],
-  }
-})
-
-const collectCity = client.createStep({
-  satisfied() {
-    return Boolean(client.getConversationState().weatherCity)
-  },
-
-  extractInfo() {
-    const city = firstOfEntityRole(client.getMessagePart(), 'city')
-
-    if (city) {
-      client.updateConversationState({
-        weatherCity: city,
-      })
-
-      console.log('User wants the weather in:', city.value)
-    }
-  },
-
-  prompt() {
-    client.addResponse('app:response:name:prompt/weather_city')
-    client.done()
-  },
-})
-
-const provideWeather = client.createStep({
-  satisfied() {
-    return false
-  },
-
-  prompt() {
-    // Need to provide weather
-    client.done()
-  },
-})
