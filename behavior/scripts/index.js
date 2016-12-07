711e767365372b3cbb3c297522aaac435b101e7d
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2016-12-07 20:04:31 +0530

    Minor Changes

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,4 +1,13 @@
 'use strict'
+const firstOfEntityRole = function(message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots
+  const entityValues = message.slots[entity]
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+
+  return valsForRole ? valsForRole[0] : null
+}
 
 exports.handle = function handle(client) {
   const sayHello = client.createStep({
@@ -66,3 +75,46 @@ exports.handle = function handle(client) {
     }
   })
 }
+
+client.runFlow({
+  classifications: {},
+  streams: {
+    main: 'getWeather',
+    hi: [sayHello],
+    getWeather: [collectCity, provideWeather],
+  }
+})
+
+const collectCity = client.createStep({
+  satisfied() {
+    return Boolean(client.getConversationState().weatherCity)
+  },
+
+  extractInfo() {
+    const city = firstOfEntityRole(client.getMessagePart(), 'city')
+
+    if (city) {
+      client.updateConversationState({
+        weatherCity: city,
+      })
+
+      console.log('User wants the weather in:', city.value)
+    }
+  },
+
+  prompt() {
+    client.addResponse('app:response:name:prompt/weather_city')
+    client.done()
+  },
+})
+
+const provideWeather = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    // Need to provide weather
+    client.done()
+  },
+})
